ee239a4dca51278c5c95e4bc8430129df346bb3c
vladimir-acer <vladimir@DESKTOP-D3T5842>
2021-03-14 21:10:33 +0500

    using "useno" template tag in storage_information

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -6,14 +6,13 @@ from .models import format_duration
 
 def storage_information_view(request):
     non_closed_visits = []
-    d = {'True': 'Да!', 'False': 'Нет'}
-    for item in Visit.objects.filter(leaved_at=None):
+    for visit in Visit.objects.filter(leaved_at=None):
         non_closed_visits.append(
             {
-                "who_entered": item.passcard.owner_name,
-                "entered_at": item.entered_at,
-                "duration": format_duration(item.get_duration()),
-                "is_strange": d[str(item.is_visit_long())]
+                "who_entered": visit.passcard.owner_name,
+                "entered_at": visit.entered_at,
+                "duration": format_duration(visit.get_duration()),
+                "is_strange": visit.is_visit_long()
             }
         )
     context = {
